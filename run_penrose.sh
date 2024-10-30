e6127e678f0238689e3c67c78a893761784efaa9
Brandon Barker <brandon.barker@gmail.com>
2024-10-30 07:35:08 -0400

    attempt to rotate log after kill (SIGTERM)

--- run_penrose.sh
+++ run_penrose.sh
@@ -3,10 +3,17 @@ eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 eval $(ssh-agent)
 
+rotate_log() {
+    mv ~/.penrose.log ~/.penrose-prev.log
+}
+
+trap rotate_log SIGTERM
+
 while true; do
-  "$PENROSE_DIR/target/release/dotpenrose" &> ~/.penrose.log
-  # RUST_BACKTRACE=full "$PENROSE_DIR/target/debug/dotpenrose" &> ~/.penrose.log
-  # start a new log file if there's an error
-  [[ $? > 0 ]] && mv ~/.penrose.log ~/.penrose-prev.log
-  export RESTARTED=true
+    "$PENROSE_DIR/target/release/dotpenrose" &> ~/.penrose.log
+    # RUST_BACKTRACE=full "$PENROSE_DIR/target/debug/dotpenrose" &> ~/.penrose.log
+    # Rotate log if there's an error or if the process was terminated
+    [[ $? > 0 ]] && mv ~/.penrose.log ~/.penrose-prev.log
+    export RESTARTED=true
+    rotate_log
 done
